881dbd55504f60fa502e9c0d263cc6965c62da34
yuanwfff <wangfeiys@163.com>
2017-02-21 08:39:04 +0800

    删去无用的依赖

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ var comments = require('./routes/comments');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 mongoose.connect('mongodb://localhost/x-codder-blog') //连接本地数据库
@@ -38,13 +34,9 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.json(err);
 });
 
 module.exports = app;
