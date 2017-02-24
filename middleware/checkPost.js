4da746a0cb8b19e541512ee8e0264a1e6235556d
yuanwfff <wangfeiys@163.com>
2017-02-24 17:18:09 +0800

    小修改

--- middleware/checkPost.js
+++ middleware/checkPost.js
@@ -2,10 +2,11 @@
  * Created by Wangfei on 2017/2/17.
  */
 module.exports = function (req, res, next) {
-    if (req.body.title && req.body.content && req.body.description && req.body.status && req.body.category && req.body.tag) {
+    if (req.body.title && req.body.content && req.body.description && req.body.category && req.body.tag) {
         next();
     } else {
-        var err = new Error('新增文章请求缺少必要的参数，文章发布失败!')
+        console.log('缺少必要的参数!');
+        var err = new Error('新增文章请求缺少必要的参数，文章发布失败!');
         err.status = 400;
         next(err);
     }
--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,7 @@ var express = require('express');
 var mongoose = require('mongoose');//导入mongoose模块
 var checkParams = require('../middleware/checkPost');//检查发布文章参数是否合法的中间件
 
-var Blog = require('../models/post');//导入模型数据模块
+var Post = require('../models/post');//导入模型数据模块
 
 var router = express.Router();
 var idCreater = 1;
@@ -15,7 +15,7 @@ var idCreater = 1;
 router.post('/', checkParams);
 router.post('/', function (req, res) {
     var now = new Date().getTime();
-    var blog = new Blog({
+    var post = new Post({
         title: req.body.title,
         content: req.body.content,
         description: req.body.description,
@@ -25,12 +25,13 @@ router.post('/', function (req, res) {
         createAt: now,
         lastUpdate: now,
         readCount: 0,
+        img: req.body.img,
         author: 'X-Codder'
     });
-    blog.save();
+    post.save();
     console.log('新增一条blog记录!');
     //res.writeHead(200,{'Content-Type':'text/json;charset=utf-8'});
-    res.json(JSON.stringify(blog));
+    res.json(JSON.stringify(post));
 })
 
 //请求文章列表，默认返回10篇，按时间降序
@@ -39,26 +40,26 @@ router.get('/', function (req, res) {
     var sortby = req.query.sortby || 'createAt';
     var order = req.query.order || 'desc';
     //该接口用于请求博文列表，将文章内容去掉，减少数据传输量
-    Blog.find({}).sort('-createAt').limit(limit).exec(function (err, blogs) {
-        var cBlogs = blogs.map(function (blog) {
-            blog.content = null;
-            return blog;
+    Post.find({}).sort('-createAt').limit(limit).exec(function (err, posts) {
+        var cposts = posts.map(function (post) {
+            post.content = null;
+            return post;
         });
-        res.json(cBlogs);
+        res.json(cposts);
     })
 });
 
 //按文章ID请求文章具体内容
 router.get('/id/:id', function (req, res, next) {
     var id = req.params.id || '';
-    Blog.findOne({_id: id}, function (err, blog) {
+    Post.findOne({_id: id}, function (err, post) {
         if (err) {
-            console.log('没有检索到请求的blog!');
+            console.log('没有检索到请求的post!');
             var err = new Error('Blog Not Found');
             err.status = 404;
             next(err);
         } else {
-            res.json(blog);
+            res.json(post);
         }
     })
 });
